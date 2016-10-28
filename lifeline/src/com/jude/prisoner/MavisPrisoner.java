f11679e1f55733a7ce5eea4ef183681dc7ff3085
吴梅 <zkkswk@foxmail.com>
2016-10-28 23:29:04 +0800

    lv3

---
+++ lifeline/src/com/jude/prisoner/MavisPrisoner.java
@@ -0,0 +1,34 @@
+package com.jude.prisoner;
+
+import com.jude.Prisoner;
+import com.jude.Manager;
+
+/**
+ * Created by Mr.Jude on 2015/10/28.
+ */
+public class MavisPrisoner implements Prisoner {
+    int totalCount;
+    int totalPerson;
+
+    @Override
+    public String getName() {
+        return "吴梅 2016210841";
+    }
+
+    @Override
+    public void begin(Manager manager,int totalPerson, int totalCount) {
+        this.totalCount = totalCount;
+        this.totalPerson = totalPerson;
+    }
+
+    @Override
+    public int take(int index, int last) {
+        return ((totalCount-last)/(index+1) + totalCount/totalPerson)/2;
+    }
+
+    @Override
+    public void result(boolean survived) {
+
+    }
+
+}
